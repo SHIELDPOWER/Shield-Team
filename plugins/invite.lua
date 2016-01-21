f24b64d0d9fcc403edb1a22d19588a1943dc05f6
ereizier <ereizerbot@gmail.com>
2016-01-21 13:05:54 +0330

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -3,9 +3,9 @@ local function callbackres(extra, success, result) -- Callback for res_user in l
   local user = 'user#id'..result.id
 	local chat = 'chat#id'..extra.chatid
 	if is_banned(result.id, extra.chatid) then -- Ignore bans
-            send_large_msg(chat, 'User is banned.')
+            send_large_msg(chat, 'User Banned By SHIELDPOWER.')
 	elseif is_gbanned(result.id) then -- Ignore globall bans
-	    send_large_msg(chat, 'User is globaly banned.')
+	    send_large_msg(chat, 'User Is Globaly Banned By SHIELDPOWER.')
 	else    
 	    chat_add_user(chat, user, ok_cb, false) -- Add user on chat
 	end
