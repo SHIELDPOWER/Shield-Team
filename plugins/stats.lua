b9fa41afe8253dac12c367852bcf5259280cc499
ereizier <ereizerbot@gmail.com>
2016-01-21 18:22:56 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -99,7 +99,7 @@ local function run(msg, matches)
   end 
   if matches[1]:lower() == "statslist" then
     if not is_momod(msg) then
-      return "For mods only !"
+      return "Mods just can use this plugin !"
     end
     local chat_id = msg.to.id
     local name = user_print_name(msg.from)
@@ -109,7 +109,7 @@ local function run(msg, matches)
   if matches[1]:lower() == "stats" then
     if not matches[2] then
       if not is_momod(msg) then
-        return "For mods only !"
+        return "Mods just can use this plugin !"
       end
       if msg.to.type == 'chat' then
         local chat_id = msg.to.id
@@ -120,16 +120,16 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "teleseed" then -- Put everything you like :)
+    if matches[2] == "shieldpwr" then -- Put everything you like :)
       if not is_admin(msg) then
-        return "For admins only !"
+        return "Mods just can use this plugin !"
       else
         return bot_stats()
       end
     end
     if matches[2] == "group" then
       if not is_admin(msg) then
-        return "For admins only !"
+        return "Mods just can use this plugin !"
       else
         return chat_stats(matches[3])
       end
@@ -138,11 +138,11 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/]([Ss]tats)$",
-    "^[!/]([Ss]tatslist)$",
-    "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/>]([Ss]tats)$",
+    "^[!/>]([Ss]tatslist)$",
+    "^[!/>]([Ss]tats) (group) (%d+)",
+    "^[!/>]([Ss]tats) (shieldpwr)",-- Put everything you like :)
+		"^[!/>]([Ss]ieldpwr)"-- Put everything you like :)
     }, 
   run = run
 }
